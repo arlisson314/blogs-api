d0cc8d57736e8e81d41521f42af8c02cbbfa6066
Arlisson N. Costa <arlisson314@gmail.com>
2022-08-29 15:37:30 -0500

    feat: cria o model e a migration da tabela category

---
+++ src/database/models/category.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const User = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category',
+  {
+    id:{ 
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'Categories',
+  });
+  return Category;
+};
+
+module.exports = User;
